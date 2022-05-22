85d977e162ef32ac59500587279961233a88ca02
amigoscode <salamikehinde345@gmail.com>
2022-05-22 18:21:09 +0100

    setting up the entire security

---
+++ src/main/java/com/practice/poll/practice/Config/JwtTokenProvider.java
@@ -0,0 +1,72 @@
+package com.practice.poll.practice.Config;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.SignatureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider { //majorly for generating and verifying jwt
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+   @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+   @Value("${app.jwtExpirationInMs}")
+   private int jwtExpirationInMs;
+
+   public String generateToken(Authentication authentication){
+
+       UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+       Date date = new Date();
+       Date expiryDate = new Date(date.getTime() + jwtExpirationInMs);
+
+       return Jwts.builder()
+               .setSubject(Long.toString(userPrincipal.getId()))
+               .setIssuedAt(new Date())
+               .setExpiration(expiryDate)
+               .signWith(SignatureAlgorithm.HS512,jwtSecret)
+               .compact();
+   }
+
+   public Long getUserIdFromJwt(String token){ //like the name implies
+       Claims claims = Jwts.parser()
+               .setSigningKey(jwtSecret)
+               .parseClaimsJws(token)
+               .getBody();
+
+       return Long.parseLong(claims.getSubject());
+   }
+
+   public Boolean validateToken(String authToken){ //validation only
+    try {
+        Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+        return true;
+    }catch (SignatureException exception){
+        logger.error("JWT SIGNATURE IS INVALID");
+
+    }catch (MalformedJwtException exception){
+        logger.error("JWT TOKEN IS INVALID");
+
+    }catch (ExpiredJwtException exception){
+        logger.error("JWT TOKEN IS EXPIRED");
+
+    }catch (UnsupportedJwtException exception){
+        logger.error("JWT TOKEN IS UNSUPPORTED");
+
+    }catch (IllegalArgumentException exception){
+        logger.error("JWT CLAIMS STRING IS EMPTY");
+    }
+
+    return false;
+   }
+
+
+}
---
+++ src/main/java/com/practice/poll/practice/Config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.practice.poll.practice.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable().exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/","/**/*.html", "/**/*.css", "/**/*.js").permitAll()
+                .antMatchers("/api/auth/**")
+
+
+    }
+}
