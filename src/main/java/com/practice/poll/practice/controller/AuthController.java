4251d682b0b72aecf68f27593bb4372585d8e6b9
amigoscode <salamikehinde345@gmail.com>
2022-05-23 04:31:27 +0100

    set up the payload..now controller

---
+++ src/main/java/com/practice/poll/practice/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.practice.poll.practice.controller;
+
+import com.practice.poll.practice.payload.LoginRequest;
+import com.practice.poll.practice.repository.RoleRepository;
+import com.practice.poll.practice.repository.UserRepository;
+import com.practice.poll.practice.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest)
+}
