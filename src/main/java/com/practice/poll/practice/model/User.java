25759322ba3cfda6f17151af931ff89052e7ba55
amigoscode <salamikehinde345@gmail.com>
2022-05-25 16:48:35 +0100

    bug free

--- src/main/java/com/practice/poll/practice/model/User.java
+++ src/main/java/com/practice/poll/practice/model/User.java
@@ -44,9 +44,9 @@ public class User extends DateAudit {
     private String password;
 
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
+//    @JoinTable(name = "user_roles",
+//            joinColumns = @JoinColumn(name = "user_id"),
+//            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
