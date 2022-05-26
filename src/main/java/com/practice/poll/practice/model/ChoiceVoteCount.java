2c1ff5e1ca02b03879e3db8986c7205456c84172
amigoscode <salamikehinde345@gmail.com>
2022-05-26 11:08:06 +0100

    set up the entire repository

---
+++ src/main/java/com/practice/poll/practice/model/ChoiceVoteCount.java
@@ -0,0 +1,23 @@
+package com.practice.poll.practice.model;
+
+public class ChoiceVoteCount {
+
+    private Long choiceId;
+    private Long voteCount;
+
+    public Long getChoiceId() {
+        return choiceId;
+    }
+
+    public void setChoiceId(Long choiceId) {
+        this.choiceId = choiceId;
+    }
+
+    public Long getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Long voteCount) {
+        this.voteCount = voteCount;
+    }
+}
--- src/main/java/com/practice/poll/practice/repository/VoteRepository.java
+++ src/main/java/com/practice/poll/practice/repository/VoteRepository.java
@@ -1,8 +1,47 @@
 package com.practice.poll.practice.repository;
 
+import com.practice.poll.practice.model.ChoiceVoteCount;
 import com.practice.poll.practice.model.Vote;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
+//Note that Many of the queries cannot be invoked using the Spring-Data-Jpa’s dynamic query methods.
+//Even if they can be invoked, they don’t generate an optimized query,they don't fire it directly from where
+// they suppose to fire them.
+
+//we’re using JPQL instead of JPA constructor expression in some of the queries to return the query result
+// in the form of
+// a custom class called ChoiceVoteCount
 public interface VoteRepository extends JpaRepository<Vote,Long> {
+   // The ChoiceVoteCount class is used in VoteRepository to return custom results from the query
 
+
+    //evoke from the choice count class the choice and the id  of the voter in the Vote class and group by id
+    @Query("SELECT NEW com.practice.poll.practice.model.ChoiceVoteCount(v.choice.id,count(v.id)) FROM Vote" +
+            "v WHERE v.poll.id = :pollId GROUP BY v.choice.id")
+    List<ChoiceVoteCount> countByPollIdInGroupByChoice(@Param("pollIds")List<Long> pollIds);
+
+    //evoke from the choice count class the id  of the voter in the Vote class and group by id
+    @Query("SELECT NEW com.practice.poll.practice.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v " +
+            "WHERE v.poll.id = :pollId GROUP BY v.choice.id")
+    List<ChoiceVoteCount> countByPollIdGroupByChoiceId(@Param("pollId") Long pollId);
+
+    //evoke from vote where the id is...then collate the user and the vote id
+    @Query("SELECT v FROM Vote v where v.user.id = :userId and v.poll.id in :pollIds")
+    List<Vote> findByUserIdAndPollIdIn(@Param("userId") Long userId, @Param("pollIds") List<Long> pollIds);
+
+    @Query("SELECT v FROM Vote v where v.user.id = :userId and v.poll.id = :pollId")
+    Vote findByUserIdAndPollId(@Param("userId") Long userId, @Param("pollId") Long pollId);
+
+    @Query("SELECT COUNT(v.id) from Vote v where v.user.id = :userId")
+    long countByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT v.poll.id FROM Vote v WHERE v.user.id = :userId")
+    Page<Long> findVotedPollIdsByUserId(@Param("userId") Long userId, Pageable pageable);
 }
+
